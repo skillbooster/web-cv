a0ab9a042f8d34d0dea9776408afdd5ef7475dac
skillboost <skillboost@protonmail.com>
2019-11-28 12:47:02 +0800

    Project CRLF to LF, Passions component update

--- src/components/Home/Passions.vue
+++ src/components/Home/Passions.vue
@@ -1,9 +1,16 @@
 <template>
   <div id="passions-area">
-    <div id="passions-box">
-      <video autoplay loop muted poster="/app-images/CodeSnippets/VueJs.jpg" id="background">
-        <source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4">
-      </video>
+    <p id="passions-list-title">Passions</p>
+    <div id="passions-list">
+      <div v-for="pas in passions"
+        :key="pas.name"
+        class="passion-item"
+        @mouseover="passionItemHover($event, pas.img_end)"
+        @mouseleave="passionItemHover($event, pas.img_start)"
+        :style="{background: backgroundImage(pas.img_start)}">
+        <p>{{ pas.name }}</p>
+        <p>{{ pas.reason }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -13,7 +20,32 @@ export default {
   name: 'Passions',
   data() {
     return {
+      passions: [
+        {
+          name: 'Martial Arts',
+          reason: 'Excitement and challenge',
+          img_start: '/app-images/Passions/muaythai_small.jpg',
+          img_end: '/app-images/Passions/warrior_small.jpg',
+        },
+        {
+          name: 'Climbing',
+          reason: 'Danger and extreme',
+          img_start: '/app-images/Passions/climbing_canyon_small.jpg',
+          img_end: '/app-images/Passions/climbing1_small.jpg',
+        },
+      ],
     };
+  },
+  methods: {
+    backgroundImage(imgUrl) {
+      return `radial-gradient(circle, transparent, black),
+        url(${imgUrl})`;
+    },
+    passionItemHover(e, imgUrl) {
+      if (e.target.classList.contains('passion-item')) {
+        e.target.style.background = this.backgroundImage(imgUrl);
+      }
+    },
   },
 };
 </script>
@@ -26,16 +58,50 @@ export default {
   margin: 0;
 }
 
-#passions-box {
-  width: 100%;
+#passions-list-title {
+  font-size: calc(12px + 2vmin);
+  font-weight: bold;
+  color: rgb(49, 48, 48);
 }
 
-#background {
-  width: 100%;
-  height: 320px;
-  z-index: -100;
-  background: black no-repeat;
-  background-size: cover;
-  opacity: .5;
+#passions-list {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.passion-item {
+  width: 60vw;
+  height: 150px;
+  border-radius: 10px;
+  margin-bottom: 5px;
+  font-weight: bold;
+  color: white;
+  text-align: start;
+  transition: .3s;
+}
+
+.passion-item:hover {
+  opacity: .8;
+}
+
+.passion-item p {
+  padding: 0;
+  margin: 0;
+  width: 90%;
+}
+
+$passion-item-text-margin-left: 20px;
+
+.passion-item p:first-child{
+  margin: 35px 0 0 $passion-item-text-margin-left;
+  font-size: calc(12px + 2vmin);
+}
+
+.passion-item p:last-child{
+  margin-left: $passion-item-text-margin-left;
+  font-size: calc(4px + 2vmin);
+  opacity: .7;
 }
 </style>
